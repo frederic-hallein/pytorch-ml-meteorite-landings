3ee4d6bceb20bff8d287cad223e04bb53cdae8c4
frederich <frederic.hallein@outlook.com>
2025-04-02 17:11:48 +0200

    added neural network

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    print("Hello, World!")
+import pandas as pd
+import numpy as np
+
+from src.common.helpers import *
+from src.neural_networks.neural_network import NeuralNetwork
+
+def main() -> None:
+    path = "data/meteorite-landings-filtered.csv"
+    df = get_filtered_csv_data(path)
+
+    df = convert_categorical_to_numerical(df)
+    print(df.head(10))
+    
+    input_size = len(df)
+    output_size = len(df["recclass"].astype("category").cat.categories)
+
+    # hyperparameters
+    learning_rate = 0.001
+    batch_size = 64
+    num_epochs = 100
+
+    device = get_device()
+    model = NeuralNetwork(input_size, output_size).to(device)
+    print(model)
+    # X = torch.tensor(df["mass"].values, device=device)
+    # print(X)
+
+
+if __name__ == "__main__": main()
+
+
+
+    
---
+++ src/neural_networks/neural_network.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, output_size) -> None:
+        super().__init__()
+        self.nn_layer_sequence = nn.Sequential(
+            nn.Linear(input_size, 6),
+            nn.ReLU(),
+            nn.Linear(6, 6),
+            nn.Softmax(dim=1),
+            nn.Linear(6, output_size)
+        )
+
+    def forward(self, x) -> None:
+        return self.nn_layer_sequence(x)
