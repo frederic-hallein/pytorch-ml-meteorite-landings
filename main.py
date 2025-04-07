e82f7b86241dc1d2551c08d3616ca35807e8f02c
frederich <frederic.hallein@outlook.com>
2025-04-07 17:58:50 +0200

    created dataloaders for train and test

--- main.py
+++ main.py
@@ -1,32 +1,89 @@
-import pandas as pd
-import numpy as np
+import torch
+from torch import optim
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
 
-from src.helper.helper import *
+from src.helper.helper import convert_categorical_to_numerical
+from src.data_loader.data_loader import get_filtered_csv_data
 from src.neural_networks.neural_network import NeuralNetwork
+
+def train(dataloader, model, loss_fn, optimizer, device) -> None:
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(torch.float32), y.to(torch.float32) 
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device) -> None:
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 def main() -> None:
     path = "data/meteorite-landings-filtered.csv"
     df = get_filtered_csv_data(path)
+    # plot_data(df)
 
-    cat_col = ["nametype", "fall"]
-    df = convert_categorical_to_numerical(df, cat_col)
-    
-    plot_data(df)
-
-    input_size = len(df)
+    input_size  = len(df)
     output_size = len(df["recclass"].astype("category").cat.categories)
+    print(input_size)
+    print(output_size)
 
-    # hyperparameters
-    learning_rate = 0.001
-    batch_size = 64
-    num_epochs = 100
+    cat_col = ['nametype', 'recclass', 'fall']
+    df = convert_categorical_to_numerical(df, cat_col)
 
-    device = get_device()
+    features = torch.tensor(df[['nametype', 'mass', 'fall', 'year', 'reclat', 'reclong']].values)
+    labels   = torch.tensor(df[['recclass']].values)
+    dataset  = TensorDataset(features, labels)
+
+    device = torch.accelerator.current_accelerator().type \
+            if torch.accelerator.is_available() else "cpu"
     print(f"Using {device} device")
 
     model = NeuralNetwork(input_size, output_size).to(device)
     print(model)
-    # X = torch.tensor(df["mass"].values, device=device)
-    # print(X)
+
+    # hyperparameters
+    learning_rate = 0.001
+    batch_size    = 64
+    epochs        = 5
+
+    # TODO : deal with NaN values
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2])
+    train_dataloader = DataLoader(train_dataset, batch_size)
+    test_dataloader  = DataLoader(test_dataset , batch_size)
+
+
+    loss_fn   = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), learning_rate)
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        # test(test_dataloader, model, loss_fn, device)
+    print("Done!")
 
 if __name__ == "__main__": main()
--- src/helper/helper.py
+++ src/helper/helper.py
@@ -3,44 +3,6 @@ import torch
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def create_filtered_csv_data(path: str) -> None:
-    """
-    Create new filterd csv dataset.
-    The following incorrectly parsed entries are filtered:
-    - Date that is before 860 CE or after 2016
-    - Latitude and longitude of 0N/0E
-
-    :param path: String name of path
-    :return: None
-    """
-    try:
-        df = pd.read_csv(path)
-    except FileNotFoundError:
-        raise AssertionError(f"File '{path}' does not exist.")
-
-    for x in df.index:
-        if df.loc[x, "year"] <= 860 or df.loc[x, "year"] >= 2016 \
-            or (df.loc[x, "reclong"] == 0.0 and df.loc[x, "reclat"] == 0.0) \
-            or (df.loc[x, "reclong"] < -180.0 or df.loc[x, "reclong"] > 180.0):
-            df.drop(x, inplace = True)
-
-    path_filtered_data = "data/meteorite-landings-filtered.csv"
-    df.to_csv(path_filtered_data, index=False)
-
-def get_filtered_csv_data(path: str) -> pd.DataFrame:
-    """ 
-    Get filtered csv data.
-
-    :param path: String name of path
-    :return: pd.DataFrame containing filtered dataset
-    """
-    if not os.path.isfile(path):
-        print(f"File '{path}' does not exist. Creating new filtered dataset from original dataset.")
-        path_original_data = "data/archive/meteorite-landings.csv" 
-        create_filtered_csv_data(path_original_data)
-        
-    return pd.read_csv(path)
-
 def convert_categorical_to_numerical(df: pd.DataFrame, col_names: list[str]) -> pd.DataFrame:
     """
     Convert the categorical data into numerical.
@@ -56,16 +18,6 @@ def convert_categorical_to_numerical(df: pd.DataFrame, col_names: list[str]) -> 
     cat_columns = df.select_dtypes(['category']).columns
     df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
     return df
-
-def get_device() -> str:
-    """
-    Get device name used to accelerate operations if present.
-    If not present, use the cpu.
-
-    :return: String name of device
-    """
-    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-    return device
 
 def _plot_pairplot(df: pd.DataFrame) -> None:
     """
